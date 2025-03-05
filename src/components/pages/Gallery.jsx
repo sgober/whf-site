aad2cf4602f7388c84d5c467c64c47f16bc11c04
sgober <sgober@mitre.org>
2025-03-05 14:20:39 -0500

    add lightbox library and hook up to gallery

--- src/components/pages/Gallery.jsx
+++ src/components/pages/Gallery.jsx
@@ -1,26 +1,40 @@
-import { Grid, useMediaQuery } from '@mui/material';
+import React, { useState } from 'react';
+import { Box, ButtonBase, Grid, useMediaQuery } from '@mui/material';
 import _ from 'lodash';
+import { Lightbox } from 'yet-another-react-lightbox';
 
-function PhotoGallery() {
-  const columns = 4;
-  const columnsByBreakpoint = { mobile: 4, tablet: 2, laptop: 1, desktop: 1 };
+function Gallery() {
   const isMobile = useMediaQuery(theme => theme.breakpoints.only('mobile'));
   const isTablet = useMediaQuery(theme => theme.breakpoints.only('tablet'));
   const images = import.meta.glob('/src/assets/images/gallery/*');
-  const chunkSize = Math.ceil(_.divide(Object.keys(images).length, isMobile ? 1 : isTablet ? 2 : 4));
-  const imageGroups = _.chunk(Object.keys(images), chunkSize);
+  const imageArray = Object.keys(images).map((image, index) => ({ src: image, index }));
+  const chunkSize = Math.ceil(_.divide(imageArray.length, isMobile ? 1 : isTablet ? 2 : 4));
+  const imageGroups = _.chunk(imageArray, chunkSize);
+  const [selectedImageIndex, setSelectedImageIndex] = useState();
 
   return (
-    <Grid container columns={columns} columnSpacing={1} rowSpacing={0.5}>
-      {imageGroups.map((imageGroup, gIndex) => (
-        <Grid item key={gIndex} {...columnsByBreakpoint}>
-          {imageGroup.map((image, iIndex) => (
-            <img key={iIndex} src={image} style={{ width: '100%' }} />
-          ))}
-        </Grid>
-      ))}
-    </Grid>
+    <React.Fragment>
+      <Grid container columns={4} columnSpacing={1}>
+        {imageGroups.map((imageGroup, gIndex) => (
+          <Grid item key={gIndex} laptop={1} mobile={4} tablet={2}>
+            <Box sx={{ display: 'flex', flexDirection: 'column', gap: 1 }}>
+              {imageGroup.map((image, iIndex) => (
+                <ButtonBase disableRipple key={iIndex} onClick={() => setSelectedImageIndex(image.index)}>
+                  <img src={image.src} style={{ width: '100%' }} />
+                </ButtonBase>
+              ))}
+            </Box>
+          </Grid>
+        ))}
+      </Grid>
+      <Lightbox
+        close={setSelectedImageIndex}
+        index={selectedImageIndex}
+        open={!_.isNil(selectedImageIndex)}
+        slides={imageArray}
+      />
+    </React.Fragment>
   );
 }
 
-export default PhotoGallery;
+export default Gallery;
