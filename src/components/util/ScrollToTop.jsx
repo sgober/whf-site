ff97b7b8be3abb091dfa460f18ee06ccf280f640
sgober <sgober@mitre.org>
2025-03-05 21:06:56 -0500

    more tweaks

--- src/components/util/ScrollToTop.jsx
+++ src/components/util/ScrollToTop.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useCallback, useEffect } from 'react';
 import { KeyboardArrowUp } from '@mui/icons-material';
 import { Box, Fab, Fade, useMediaQuery, useScrollTrigger } from '@mui/material';
 import { useLocation } from 'react-router-dom';
@@ -13,9 +13,9 @@ export default function ScrollToTop() {
     threshold: 100
   });
 
-  const scrollToTop = () => {
+  const scrollToTop = transition => {
     if (isMobile) {
-      window.scrollTo(0, 0);
+      window.scrollTo({ top: 0, left: 0, behavior: transition ? 'smooth' : 'instant' });
     } else {
       document.getElementById('body').scrollTop = 0;
     }
@@ -28,7 +28,7 @@ export default function ScrollToTop() {
   return (
     isMobile && (
       <Fade in={trigger}>
-        <Box onClick={scrollToTop} role="presentation" sx={{ position: 'fixed', bottom: 25, right: 25 }}>
+        <Box onClick={() => scrollToTop(true)} role="presentation" sx={{ position: 'fixed', bottom: 25, right: 25 }}>
           <Fab aria-label="scroll back to top" size="medium">
             <KeyboardArrowUp />
           </Fab>
