8ec30e8034afb5d29644324e690c62f84adcb08d
sgober <sgober@mitre.org>
2025-03-09 20:42:24 -0400

    add erin info to team page

--- src/components/pages/Team.jsx
+++ src/components/pages/Team.jsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 import { Box, Link, Typography } from '@mui/material';
 import AbbyImage from 'assets/images/abby.jpg';
 import CaseyImage from 'assets/images/casey.jpg';
-import ErinImage from 'assets/images/erin.jpg';
+import ErinImage from 'assets/images/team/erin.jpeg';
 import TeamMember from 'components/util/TeamMember';
 
 function Team({ classes }) {
@@ -21,14 +21,20 @@ function Team({ classes }) {
         </Typography>
       </TeamMember>
       <TeamMember image={AbbyImage} name="Abigail Sullivan" title="CFO">
-        <Typography> Abby's bio here</Typography>
-      </TeamMember>
-      <TeamMember image={ErinImage} name="Erin Shafer" title="Barn Manager">
         <Typography>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
           aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
           Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
           sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+        </Typography>
+      </TeamMember>
+      <TeamMember image={ErinImage} name="Erin Shafer" title="Barn Manager">
+        <Typography>
+          Originally from Madison, Connecticut, Erin graduated from Roger Williams University in 2020 which prompted her move
+          to Bristol, Rhode Island where she managed a hunter/jumper farm. Erin loved caring for the horses and catering to
+          their individual needs, peaking her interest in veterinary care. Erin worked as an equine technician assisting with
+          routine appointments, emergencies and surgeries. Erin loved working with both hospital patients and quarantine
+          mares which helped her find her home at Walnut Hill Farm with her horse Linus.
         </Typography>
       </TeamMember>
     </Box>
