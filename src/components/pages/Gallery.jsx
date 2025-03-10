e890ebccdb219c951b3aa69e0d2bb194903fca61
sgober <sgober@mitre.org>
2025-03-10 19:40:34 -0400

    update gallery page

--- src/components/pages/Gallery.jsx
+++ src/components/pages/Gallery.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { Box, ButtonBase, Grid, useMediaQuery } from '@mui/material';
 import _ from 'lodash';
@@ -8,18 +8,25 @@ import { chunk } from 'utils/chunk';
 function Gallery({ classes }) {
   const isMobile = useMediaQuery(theme => theme.breakpoints.only('mobile'));
   const isTablet = useMediaQuery(theme => theme.breakpoints.only('tablet'));
-  const images = import.meta.glob('/public/gallery/*');
-  const imageArray = Object.keys(images).map((image, index) => ({ src: image.replace('/public', ''), index }));
-  const imageGroups = chunk(imageArray, 4, isMobile ? 1 : isTablet ? 2 : 4);
+  const [imageArray, setImageArray] = useState([]);
+  const [imageGroups, setImageGroups] = useState([]);
   const [selectedImageIndex, setSelectedImageIndex] = useState();
+
+  useEffect(() => {
+    const images = import.meta.glob('/public/gallery/*');
+    const array = _.shuffle(Object.keys(images)).map((image, index) => ({ src: image.replace('/public', ''), index }));
+    const groups = chunk(array, 4, isMobile ? 1 : isTablet ? 2 : 4);
+    setImageArray(array);
+    setImageGroups(groups);
+  }, [isMobile, isTablet]);
 
   return (
     <Box className={classes} sx={{ maxWidth: 1200 }}>
       <Grid container columns={4} columnSpacing={1}>
-        {imageGroups.map((imageGroup, gIndex) => (
+        {imageGroups.map((group, gIndex) => (
           <Grid item key={gIndex} laptop={1} mobile={4} tablet={2}>
             <Box sx={{ display: 'flex', flexDirection: 'column', gap: 1 }}>
-              {imageGroup.map((image, iIndex) => (
+              {group.map((image, iIndex) => (
                 <ButtonBase disableRipple key={iIndex} onClick={() => setSelectedImageIndex(image.index)}>
                   <img src={image.src} style={{ width: '100%', maxHeight: isMobile ? 450 : 350, objectFit: 'cover' }} />
                 </ButtonBase>
