322a83586418e14cb3ec478e72feef834699de94
sgober <sgober@mitre.org>
2025-03-10 20:03:02 -0400

    update team page

--- src/components/pages/Team.jsx
+++ src/components/pages/Team.jsx
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 import { Box, Link, Typography, useMediaQuery } from '@mui/material';
-import AbbyImage from 'assets/images/abby.jpg';
+import AbbyImage from 'assets/images/team/abby.jpeg';
 import CaseyImage from 'assets/images/casey.jpg';
 import ErinImage from 'assets/images/team/erin.jpeg';
 import TeamMember from 'components/util/TeamMember';
