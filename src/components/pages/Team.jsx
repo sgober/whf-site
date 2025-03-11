fe2c21e2987ca62bb7c54260dcf2bae2f6be9dda
sgober <sgober@mitre.org>
2025-03-10 20:27:22 -0400

    abby bio

--- src/components/pages/Team.jsx
+++ src/components/pages/Team.jsx
@@ -25,10 +25,12 @@ function Team({ classes }) {
         </TeamMember>
         <TeamMember image={AbbyImage} name="Abigail Sullivan" title="CFO">
           <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-            magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+            Beginning her riding career with a focus on eventing, Abigail furthered her skills through young adulthood,
+            expanding her knowledge of diverse training approaches and methodologies. With a strong emphasis on biomechanics,
+            Abigail brings a unique skill set to her training and teaching of both horse and rider. Holding a Master’s degree
+            in Occupational Therapy and having worked in the mental health field alongside her equestrian ventures, Abigail's
+            expertise helps riders improve posture, balance, and alignment, while promoting comfort, safety, and a deeper
+            connection with their horses.
           </Typography>
         </TeamMember>
         <TeamMember image={ErinImage} name="Erin Shafer" title="Barn Manager">
